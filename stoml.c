f63471fd6247c904ee2830d9bf04954c7c309247
Ole Myhra <olem@triggerspeed.com>
2024-11-24 12:28:27 +0100

    Added trim()

--- stoml.c
+++ stoml.c
@@ -1,9 +1,11 @@
 #include "stoml.h"
 
-static void create_node(char *line, stoml_data *node);
+#define TRIM_MAX_LENGTH 100
+
+static void create_node(char *line, stoml_data **node);
 static int create_hash_code(const char *key);
 static void insert_hashtable(stoml_data *data[], const int length, stoml_data *data_item);
-
+static void trim(char *string);
 
 int stoml_read(stoml_data *data[], const int length, FILE *stream) {
 
@@ -28,7 +30,7 @@ int stoml_read(stoml_data *data[], const int length, FILE *stream) {
 		} else if (c == '\n') {
 			if (key_line) {
 				key_line_data[key_line_data_index] = '\0';
-				create_node(key_line_data, node);
+				create_node(key_line_data, &node);
 				if (node != NULL)
 					insert_hashtable(data, length, node);
 			}
@@ -49,7 +51,6 @@ int stoml_read(stoml_data *data[], const int length, FILE *stream) {
 				key_line_data_index++;
 		} 
 
-		printf("%c", c);
 		c = getc(stream);
 	}
 
@@ -57,8 +58,16 @@ int stoml_read(stoml_data *data[], const int length, FILE *stream) {
 }
 
 
-static void create_node(char *line, stoml_data *node) {
+static void create_node(char *line, stoml_data **node) {
+	char *key = NULL;
+	char *value = NULL;
 
+	key = strtok(line, "=");
+	value = strtok(NULL, "=");
+	trim(key);
+	trim(value);
+	printf("Key: %s, value: %s\n", key, value);
+	
 
 }
 
@@ -95,4 +104,23 @@ static void insert_hashtable(stoml_data *data[], const int length,  stoml_data *
 	}
 
 	data[hashtable_index] = data_item;
+}
+
+
+static void trim(char *string) {
+	short i = 0, z = 0;
+	char tmp_string[TRIM_MAX_LENGTH];
+	memset(tmp_string, 0, sizeof(tmp_string));
+
+	while (string[i] != 0x00) {
+		if (string[i] != 0x20 && string[i] != 0x22) {	/* 0x20 = Space, 0x22 = " */
+			tmp_string[z] = string[i];
+			z++;
+		}
+		i++;
+	}
+	tmp_string[z] = '\0';
+	
+	strcpy(string, tmp_string);
+
 }
